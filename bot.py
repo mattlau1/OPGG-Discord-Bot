59bf51fba8ca58ee28aacad7c67ef7e6e348c68c
Matthew Lau <matthew.lau78@gmail.com>
2020-12-30 01:38:59 +1100

    Build command implementation

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ jdaobot for Discord
 
 Written in Python 3.8.5 64-bit
 
+Usage: python3 bot.py
+
 Features:
     - jdaomode on [Usage: !jdaomode]
         - changes avatar to jdao1's avatar
@@ -13,18 +15,24 @@ Features:
         - changes avatar to Kevin Nguyen
         - changes nickname to Kevin Nguyen
         - sends "kys echau" on every message [ITS A JOKE]
-    
+    - League of Legends champion build
+        - uses http://lol.lukegreen.xyz/ api to scrape opgg data
+
     WIP:
-        - !opgg [name]
+        - !build [lane] [champion]
+        - Storing jdaomode in pickle file
 '''
+
 import discord
 import re
 from random import randint
-from scrapeGG import scrapeGG
+import requests
+import json
+import urllib.request
 from secret_token import token_class
 
-token = token_class()
-TOKEN = token.get_token()
+
+TOKEN = token_class().get_token()
 
 jono_quotes = [
     'ded lmao', 'lmao', 'ya', 'ayy lmao', 'whoopsie', 'bruh', 'bruh', 'bruh',
@@ -33,14 +41,19 @@ jono_quotes = [
 client = discord.Client()
 jdaomode = True
 
-# load profile pics
-pfp1_path = "avatars/kevin.png"
-fp1 = open(pfp1_path, 'rb')
-pfp1 = fp1.read()
-
-pfp2_path = "avatars/jdao.png"
-fp2 = open(pfp2_path, 'rb')
-pfp2 = fp2.read()
+def get_pfp(user):
+    '''
+    returns a profile picture based on given user
+    '''
+    # "avatars/kevin.png"
+    dir1 = "avatars/kevin.png"
+    dir2 = "avatars/jdao.png"
+    if user == 'kevin':
+        fp = open(dir1, 'rb')
+        return fp.read()
+    else:
+        fp = open(dir2, 'rb')
+        return fp.read()
 
 @client.event
 async def on_message(message):
@@ -50,26 +63,45 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!opgg'):
-        summoner = message.content.split(' ')
-        if len(summoner) < 2:
-            await channel.send(f'Usage: !opgg [name]')
+    if message.content.startswith('!build'):
+        msg = message.content.split(' ')
+        if len(msg) < 3:
+            await channel.send(f'Usage: !build [lane] [champion]')
         else:
-            await channel.send(f'{summoner[1]}')
+            await channel.send(f'lane: {msg[1]}')
+            await channel.send(f'champ: {msg[2]}')
+            
+            build_url = f'http://lol.lukegreen.xyz/build/{msg[1]}/{msg[2]}'
+            build = ''
+            with urllib.request.urlopen(build_url) as url:
+                data = json.loads(url.read().decode())
+                for num in range(1, 6):
+                    build += f'Build {num}: '
+                    for item in data[f'build_{num}']:
+                        build += (item.lstrip("(\"\'")) + ', '
+                    
+                   
+                    build += '\n'
+                    
+                   
+    
+            
+            await channel.send(build)
 
-        
+                    
+            
 
     elif message.content.startswith('!jdaomode'):
         if jdaomode == True:
             jdaomode = False
             await channel.send('JDAOMODE DEACTIVATED')
             await message.guild.get_member(client.user.id).edit(nick='Kevin Nguyen')
-            await client.user.edit(password=None, avatar=pfp1)
-        elif jdaomode == False:
+            await client.user.edit(password=None, avatar=get_pfp('kevin'))
+        else:
             jdaomode = True 
             await channel.send('JDAOMODE ACTIVATED')
             await message.guild.get_member(client.user.id).edit(nick='jdao1')
-            await client.user.edit(password=None, avatar=pfp2)
+            await client.user.edit(password=None, avatar=get_pfp('jdao'))
 
     elif message.content.startswith('im ') and jdaomode == True:
         regex = re.sub('(im )', 'hi ', message.content, 1, flags=re.I)
