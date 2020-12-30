7dcd0be0ee319f01e94857617e0fe00bb267876b
Matthew Lau <matthew.lau78@gmail.com>
2020-12-31 00:16:29 +1100

    pylint fixes

--- bot.py
+++ bot.py
@@ -11,44 +11,43 @@ Features:
         - sends top 5 builds for champion in specified lane
 '''
 
-import discord
-import re
-from random import randint
-import requests
 import json
 import urllib.request
-from secret.secret_token import token_class
-import asyncio
+import discord
+import requests
 from discord.ext import commands
+from secret.secret_token import token_class
 
 bot = commands.Bot(command_prefix='/')
 
 @bot.event
 async def on_ready():
+    '''
+    Prints to owner in console when bot is ready.
+    '''
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
-@bot.command()
-async def build(ctx, *args):
+@bot.command(aliases=['build'])
+async def build_cmd(ctx, *args):
     '''
     Build Command
     Lists builds for champion from op.gg
 
     Usage: /build [lane] [champion]
     '''
-    print(f'Build cmd detected')
-    await ctx.send("hi")
+    print('Build cmd detected')
     if len(args) != 2:
-        await ctx.send(f'Usage: /build [lane] [champion]')
+        await ctx.send('Usage: /build [lane] [champion]')
     else:
         build_url = f'http://lol.lukegreen.xyz/build/{args[0]}/{args[1]}'
         build = ''
         if requests.get(build_url).status_code != 500:
             await ctx.send(f'Lane: {args[0]}')
             await ctx.send(f'Champ: {args[1]}')
-            
+
             with urllib.request.urlopen(build_url) as url:
                 data = json.loads(url.read().decode())
                 for num in range(1, 6):
@@ -59,13 +58,16 @@ async def build(ctx, *args):
 
             await ctx.send(build)
         else:
-            await ctx.send(f'Check Spelling u idiot')
+            await ctx.send('Check Spelling u idiot')
 
 bot.remove_command('help')
-@bot.command()
-async def help(ctx):
+@bot.command(aliases=['help'])
+async def help_cmd(ctx):
+    '''
+    Sends all commands to user
+    '''
     print("help command triggered")
-    embed=discord.Embed(title="Commands", color=0x0f7ef5)
+    embed = discord.Embed(title="Commands", color=0x0f7ef5)
     embed.set_thumbnail(url="https://i.ibb.co/sHC7w0d/Screenshot-1.jpg")
     embed.add_field(
         name="/build [ top | mid | jg | adc | sup ] [champion]",
@@ -73,4 +75,4 @@ async def help(ctx):
     )
     await ctx.send(embed=embed)
 
-bot.run(token_class().get_token())    
+bot.run(token_class().get_token())
