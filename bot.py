f88cf60fd79fe4422e2d377bb28292d24488aee4
Matthew Lau <matthew.lau78@gmail.com>
2020-12-30 21:53:34 +1100

    oop refactor complete but not working xd

--- bot.py
+++ bot.py
@@ -41,33 +41,124 @@ jono_quotes = [
 client = discord.Client()
 jdaomode = False
 
-# class commands():
-#     def build_cmd(self, channel, message):
-#         '''
-#         Build Command
-#         Lists builds for champion from op.gg
 
-#         Usage: /build [lane] [champion]
-#         '''
-#         msg = message.content.split(' ')
-#         if len(msg) < 3:
-#             await channel.send(f'Usage: /build [lane] [champion]')
-#         else:
-#             await channel.send(f'Lane: {msg[1]}')
-#             await channel.send(f'Champ: {msg[2]}')
-            
-#             build_url = f'http://lol.lukegreen.xyz/build/{msg[1]}/{msg[2]}'
-#             build = ''
-#             with urllib.request.urlopen(build_url) as url:
-#                 data = json.loads(url.read().decode())
-#                 for num in range(1, 6):
-#                     build += f'Build {num}: '
-#                     for item in data[f'build_{num}']:
-#                         build += (item.lstrip("(\"\'")) + ', '
-                    
-#                     build += '\n'
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
+client.run(TOKEN)
 
-#             await channel.send(build)
+class command():
+    def __init__(self, message):
+        self.message = message
+        self.channel = message.channel
+
+    async def build_cmd(self):
+        '''
+        Build Command
+        Lists builds for champion from op.gg
+
+        Usage: /build [lane] [champion]
+        '''
+        print(f'Build cmd detected, self.message{self.message}')
+        msg = self.message.content.split(' ')
+        if len(msg) < 3:
+            await self.channel.send(f'Usage: /build [lane] [champion]')
+        else:
+            build_url = f'http://lol.lukegreen.xyz/build/{msg[1]}/{msg[2]}'
+            build = ''
+            if requests.get(build_url).status_code != 500:
+                await self.channel.send(f'Lane: {msg[1]}')
+                await self.channel.send(f'Champ: {msg[2]}')
+                
+                with urllib.request.urlopen(build_url) as url:
+                    data = json.loads(url.read().decode())
+                    for num in range(1, 6):
+                        build += f'Build {num}: '
+                        for item in data[f'build_{num}']:
+                            build += (item.lstrip("(\"\'")) + ', '
+                        build += '\n'
+
+                await self.channel.send(build)
+            else:
+                await self.channel.send(f'Check Spelling u idiot')
+        
+    async def jdaomode_cmd(self, jdaomode):
+        '''
+        Jdaomode Command
+
+        - jdaomode on 
+            - changes avatar to jdao1's avatar
+            - changes nickname to jdao1
+            - sends random jdao1 quote on every message
+            - sends ``"hi ____"`` if ``"i'm ____"`` is detected
+        - jdaomode off 
+            - changes avatar to Kevin Nguyen
+            - changes nickname to Kevin Nguyen
+            - stops spamming
+
+        Usage: /jdaomode
+        '''
+        if jdaomode == True:
+            jdaomode = False
+            await self.channel.send('JDAOMODE DEACTIVATED')
+            await self.message.guild.get_member(client.user.id).edit(nick='Kevin Nguyen')
+            await client.user.edit(password=None, avatar=get_pfp('kevin'))
+        else:
+            jdaomode = True 
+            await self.channel.send('JDAOMODE ACTIVATED')
+            await self.message.guild.get_member(client.user.id).edit(nick='jdao1')
+            await client.user.edit(password=None, avatar=get_pfp('jdao'))
+
+    async def help_cmd(self):
+        await self.channel.send('''
+        Features:
+        - jdaomode on ``[Usage: /jdaomode]``
+            - changes avatar to jdao1's avatar
+            - changes nickname to jdao1
+            - sends random jdao1 quote on every message
+            - sends ``"hi ____"`` if ``"i'm ____"`` is detected
+        - jdaomode off ``[Usage: /jdaomode]``
+            - changes avatar to Kevin Nguyen
+            - changes nickname to Kevin Nguyen
+            - stops spamming
+        - League of Legends champion build ``[Usage: /build [lane] [champion]]``
+            - sends top 5 builds for champion in specified lane
+        ''')
+
+    async def im_cmd(self):
+        regex = re.sub('(im )', 'hi ', self.message.content, 1, flags=re.I)
+        await self.channel.send(regex)
+
+    async def quote_cmd(self):
+        rand = randint(0, len(jono_quotes) - 1)
+        await self.channel.send(jono_quotes[rand])
+
+@client.event
+async def on_message(message):
+    global jdaomode
+    cmds = command(message)
+    
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/build'):
+        cmds.build_cmd()
+
+    elif message.content.startswith('/jdaomode'):
+        cmds.jdaomode_cmd(jdaomode)
+
+    elif message.content.startswith('/help'):
+        cmds.help_cmd()
+
+    elif message.content.startswith('im ') and jdaomode == True:
+        cmds.im_cmd()
+
+    elif jdaomode == True:
+        cmds.quote_cmd()
 
 def get_pfp(user):
     '''
@@ -83,89 +174,4 @@ def get_pfp(user):
         fp = open(dir2, 'rb')
         return fp.read()
 
-@client.event
-async def on_message(message):
-    global jdaomode
-    channel = message.channel
-    # cmds = commands(channel, message)
-    
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('/build'):
-        '''
-        Build Command
-        Lists builds for champion from op.gg
-
-        Usage: /build [lane] [champion]
-        '''
-        msg = message.content.split(' ')
-        if len(msg) < 3:
-            await channel.send(f'Usage: /build [lane] [champion]')
-        else:
-            await channel.send(f'Lane: {msg[1]}')
-            await channel.send(f'Champ: {msg[2]}')
-            
-            build_url = f'http://lol.lukegreen.xyz/build/{msg[1]}/{msg[2]}'
-            build = ''
-            with urllib.request.urlopen(build_url) as url:
-                data = json.loads(url.read().decode())
-                for num in range(1, 6):
-                    build += f'Build {num}: '
-                    for item in data[f'build_{num}']:
-                        build += (item.lstrip("(\"\'")) + ', '
-                    
-                    build += '\n'
-
-            await channel.send(build)
-
-    elif message.content.startswith('/jdaomode'):
-        if jdaomode == True:
-            jdaomode = False
-            await channel.send('JDAOMODE DEACTIVATED')
-            await message.guild.get_member(client.user.id).edit(nick='Kevin Nguyen')
-            await client.user.edit(password=None, avatar=get_pfp('kevin'))
-        else:
-            jdaomode = True 
-            await channel.send('JDAOMODE ACTIVATED')
-            await message.guild.get_member(client.user.id).edit(nick='jdao1')
-            await client.user.edit(password=None, avatar=get_pfp('jdao'))
-
-    elif message.content.startswith('/help'):
-        await channel.send(
-        '''
-        Features:
-    - jdaomode on ``[Usage: /jdaomode]``
-        - changes avatar to jdao1's avatar
-        - changes nickname to jdao1
-        - sends random jdao1 quote on every message
-        - sends ``"hi ____"`` if ``"i'm ____"`` is detected
-    - jdaomode off ``[Usage: /jdaomode]``
-        - changes avatar to Kevin Nguyen
-        - changes nickname to Kevin Nguyen
-        - stops spamming
-    - League of Legends champion build ``[Usage: /build [lane] [champion]]``
-        - sends top 5 builds for champion in specified lane
-        '''
-        )
-    elif message.content.startswith('im ') and jdaomode == True:
-        regex = re.sub('(im )', 'hi ', message.content, 1, flags=re.I)
-        await channel.send(regex)
-
-    elif jdaomode == True:
-        rand = randint(0, len(jono_quotes) - 1)
-        await channel.send(jono_quotes[rand])
-
-    
-    
-    
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-    
-
-client.run(TOKEN)
